a68a9d2e6a64ff21b8647d0eaae9478a91153927
Ondrej Flidr <flidr@webeo.cz>
2018-05-23 21:02:01 +0200

    Vypisuje namerene hodnoty i na LCD

--- teramon.py
+++ teramon.py
@@ -6,7 +6,6 @@ import RPi_I2C_driver
 from time import *
 
 lcd = RPi_I2C_driver.lcd()
-lcd.lcd_display_string("Hello world!", 1)
 
 while True:
 
@@ -14,5 +13,8 @@ while True:
     # model: 11, 22
     # GPIO pin podle dokumentace Raspberry
     humidity, temperature = Adafruit_DHT.read_retry(11, 18)
+    lcd.lcd_clear()
+    lcd.lcd_display_string('Temp: {0:01f} C'.format(temperature), 1)
+    lcd.lcd_display_string('Humidity: {1:0.1f} %'.format(humidity), 2)
 
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
