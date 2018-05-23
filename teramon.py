a469e1f798d18705d53bbb825e9bb36fa78cc2c0
Ondrej Flidr <flidr@webeo.cz>
2018-05-23 20:53:35 +0200

    Umisteni mojeho testovaciho cidla, at git nerve na konflikty

--- teramon.py
+++ teramon.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import sys
 import Adafruit_DHT
@@ -13,6 +13,6 @@ while True:
     # read_retry(model, gpio pin)
     # model: 11, 22
     # GPIO pin podle dokumentace Raspberry
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 18)
 
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
