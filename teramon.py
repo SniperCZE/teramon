0b57286b96134cbc3964bacc471dc13a3a8513f0
Ondrej Flidr <flidr@webeo.cz>
2018-05-23 20:42:42 +0200

    komentare k parametrum DHT knihovny

--- teramon.py
+++ teramon.py
@@ -5,6 +5,9 @@ import Adafruit_DHT
 
 while True:
 
+    # read_retry(model, gpio pin)
+    # model: 11, 22
+    # GPIO pin podle dokumentace Raspberry
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
