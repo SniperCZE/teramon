0f13f69a9e9f3a9509eb2cf62e474acbf2e7b0c9
Ondrej Flidr <flidr@webeo.cz>
2018-05-24 18:07:04 +0200

    Ovladani relatek spinajicich svetlo a teplo

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Software pro hlidani teploty a vlhkosti v terariu. Vytvoreno pro Raspberry PI, j
 * Ovladaci knihovna DHT11: `git clone https://github.com/adafruit/Adafruit_Python_DHT.git`
 * Navod na zapojeni cidla: http://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
 * Navod na I2C LCD: http://www.circuitbasics.com/raspberry-pi-i2c-lcd-set-up-and-programming/
+* Knihovna GPIOZero: https://gpiozero.readthedocs.io/
 
 ## Použité piny raspberry
 
---
+++ teramon-rele.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys
+import teramon
+import time
+from gpiozero import OutputDevice
+
+tmon = teramon.teramon()
+
+PIN_RELE_SVETLO = 23
+PIN_RELE_TEPLO = 24
+
+dataSklo = tmon.mereni(teramon.PIN_CIDLO_SKLO)
+dataLampa = tmon.mereni(teramon.PIN_CIDLO_LAMPA)
+dataDzungle = tmon.mereni(teramon.PIN_CIDLO_DZUNGLE)
+
+aktualniHodina = time.strftime('%H')
+jeDen = (aktualniHodina >= 8) and (aktualniHodina <= 21)
+
+# regulace teplozarivky
+stavTeplo = False
+if (dataLampa['temp'] < 30) and jeDen:
+    stavTeplo = True
+if ((dataSklo['temp'] < 21) or (dataDzungle['temp'] < 21)) and jeDen:
+    stavTeplo = True
+if ((dataSklo['temp'] < 18) or (dataLampa['temp'] < 18) or (dataDzungle['temp'] < 18)) and (jeDen == False):
+    stavTeplo = True
+
+# regulace svetla
+stavSvetlo = jeDen
+
+releSvetlo = OutputDevice(PIN_RELE_SVETLO)
+releTeplo = OutputDevice(PIN_RELE_TEPLO)
+
+if (stavSvetlo):
+    releSvetlo.on()
+else:
+    releSvetlo.off()
+
+if (stavTeplo):
+    releTeplo.on()
+else:
+    releTeplo.off()
