2d0de1c6bbe7d7701fb0b016540e8116aaf3aaac
Ondrej Flidr <flidr@webeo.cz>
2018-05-27 15:48:00 +0200

    Stupne se musi zadavat pres chr

--- teramon-lcd.py
+++ teramon-lcd.py
@@ -18,7 +18,9 @@ def vypisLcd(pozice, data, lcd):
         lcd.backlight(0)
     lcd.lcd_display_string(pozice, 1)
     lcd.lcd_display_string_pos("{0:0.0f}:{1:0.0f}".format(hodina, minuta), 1, 11)
-    lcd.lcd_display_string('T: {0:0.0f}°C RH: {1:0.0f}%'.format(data['temp'], data['hum']), 2)
+    lcd.lcd_display_string('T: {0:0.0f}', 2)
+    lcd.lcd_display_string_pos(chr(2230), 2, 5)
+    lcd.lcd_display_string_pos('C RH: {1:0.0f}%'.format(data['temp'], data['hum']), 2, 6)
 
 lcd = RPi_I2C_driver.lcd()
 
