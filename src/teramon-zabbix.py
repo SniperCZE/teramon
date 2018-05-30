cae3a6e3676e37352bf24dcba99602653c3b4170
Ondrej Flidr <flidr@webeo.cz>
2018-05-30 18:55:17 +0200

    Zabbix check pouziva konfiguraci pro cidla

--- src/teramon-zabbix.py
+++ src/teramon-zabbix.py
@@ -2,10 +2,16 @@
 
 import sys
 import teramon
+import json
+
+json_data = open("./teramon.json").read()
+konfigurace = json.loads(json_data)
+
+list_cidel = ', '.join(konfigurace['senzory'].keys())
 
 if len(sys.argv) != 3:
     print("Pouziti: teramon-zabbix.py POZICE VELICINA")
-    print("POZICE: SKLO, LAMPA, DZUNGLE")
+    print("POZICE: {0}".format(list_cidel))
     print("VELICINA: hum, temp")
     quit()
 
@@ -13,12 +19,8 @@ tmon = teramon.teramon()
 
 pozice = sys.argv[1]
 
-if pozice == "SKLO":
-    data = tmon.mereni(tmon.PIN_CIDLO_SKLO)
-elif pozice == "LAMPA":
-    data = tmon.mereni(tmon.PIN_CIDLO_LAMPA)
-elif pozice == "DZUNGLE":
-    data = tmon.mereni(tmon.PIN_CIDLO_DZUNGLE)
+if pozice in konfigurace['senzory'].keys():
+    data = tmon.mereni(konfigurace['senzory'][pozice]['gpio'])
 else:
     data = { 'hum' : 0, 'temp' : 0 }
 
